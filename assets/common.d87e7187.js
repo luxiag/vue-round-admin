d8789e312fc7c156453f9c7800e2173c6adf7946
卢祥 <example@qq.com>
2023-01-10 16:37:17 +0800

    deploy

---
+++ assets/common.d87e7187.js
@@ -0,0 +1,1 @@
+const e={okText:"OK",closeText:"Close",cancelText:"Cancel",loadingText:"Loading...",saveText:"Save",delText:"Delete",resetText:"Reset",searchText:"Search",queryText:"Search",inputText:"Please enter",chooseText:"Please choose",redo:"Refresh",back:"Back",light:"Light",dark:"Dark"};export{e as default};
---
+++ assets/dashboard.e3354f37.js
@@ -0,0 +1,1 @@
+const u={dashboard:"\u4EEA\u8868\u76D8",workbench:"\u5DE5\u4F5C\u53F0",analysis:"\u5206\u6790\u9875"};export{u as default};
